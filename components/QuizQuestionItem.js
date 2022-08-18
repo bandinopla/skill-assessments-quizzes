0785e544c2cc7d4dfae6b4c2a6dfaf701aa1c579
Bandinopla <71508858+bandinopla@users.noreply.github.com>
2022-08-18 15:24:04 -0300

    bugfix

--- components/QuizQuestionItem.js
+++ components/QuizQuestionItem.js
@@ -1,22 +1,36 @@
-import { useContext } from "react";
+import { createContext, useContext } from "react";
 import { QuizContext } from "../lib/QuizContext";
 
+/**
+ * Since we are parsing a markup, it could happen that the options on a quiz have also markup of lists inside of them...
+ * So, since these lists will be childrens of this option, we'll create a context per each list item if non exists already
+ * and if it does exist, it will mean we are a child list and should not be counted as a quiz answer option.
+ */
+const OptionItemContext = createContext(-1);
 
 /**
- * This items represents one option item of the many available in a question. 
+ * This items represents one option item of the many available in a question.
  */
-export default function QuizQuestionItem({ children }) {
-    
-    /**
-     * @type {import('../lib/QuizContext').ContextType}
-     */
+export default function QuizQuestionItem({ children, ...props }) {
+	/**
+	 * @type {import('../lib/QuizContext').ContextType}
+	 */
 	const quizContext = useContext(QuizContext);
-	const myIndex = ++quizContext.optionIndex;
+	const optionIndex = useContext(OptionItemContext);
+    const myIndex = props.index; //++quizContext.optionIndex;
+
+    //
+    // we are a child of the option
+    //
+	if ( optionIndex != -1) {
+		return <>{children}</>;
+	} 
+	
 	let bg = "bg-white";
- 
-    //
-    // Was the question answered?
-    //
+
+	//
+	// Was the question answered?
+	//
 	if (quizContext.answered > -1) {
 		if (quizContext.answer == myIndex) {
 			bg = "bg-green-300";
@@ -25,6 +39,9 @@ export default function QuizQuestionItem({ children }) {
 		}
 	}
 
+    //
+    // When the use clicks the option to respond...
+    //
 	const onClick = () => {
 		if (quizContext.answered < 0) {
 			quizContext.onAnswer(myIndex);
@@ -32,9 +49,14 @@ export default function QuizQuestionItem({ children }) {
 	};
 
 	return (
-		<li className={"p-3 shadow-md m-2 cursor-pointer " + bg} onClick={onClick}>
-			{" "}
-			{children}{" "}
-		</li>
+		<OptionItemContext.Provider value={myIndex}>
+			<li
+				className={"p-3 shadow-md m-2 cursor-pointer " + bg}
+				onClick={onClick}
+			>
+				{" "}
+				{children}{" "}
+			</li>
+		</OptionItemContext.Provider>
 	);
 }
